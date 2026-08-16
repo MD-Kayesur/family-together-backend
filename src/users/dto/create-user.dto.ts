2e9e65bd2c5cd7fd84d25968ede25b81e9bd85a1
MD-Kayesur <rmdkayesur@gmail.com>
2026-08-16 22:10:46 +0600

    initialize Prisma user schema and create user DTO

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,10 @@
-export class CreateUserDto {}
+export class CreateUserDto {
+  email: string;
+  fullName: string;
+  password?: string;
+  avatarUrl?: string;
+  phoneNumber?: string;
+  bio?: string;
+  role?: 'OWNER' | 'ADMIN' | 'MEMBER' | 'VIEWER';
+}
+
