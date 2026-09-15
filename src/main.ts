fcf17c53d449cc66bec590a6eb60542f7b807ffe
MD-Kayesur <rmdkayesur@gmail.com>
2026-09-16 00:34:31 +0600

    Increase Express JSON body payload limit to 500mb to resolve 413 Payload Too Large

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
@@ -10,6 +11,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  app.use(json({ limit: '500mb' }));
+  app.use(urlencoded({ extended: true, limit: '500mb' }));
 
   app.use(cookieParser());
 
